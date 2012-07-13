61d6bc056281d58705ca90fd4d94ae15f90fb5a1
Libertad Speranza <libertadsperanza@gmail.com>
2012-07-13 16:44:54 -0300

    Pequeño refactoring y busqueda secuencial

---
+++ src/groovy/tesis/test/EditDistanceTest.groovy
@@ -0,0 +1,13 @@
+package tesis.test
+
+import com.sun.xml.internal.bind.v2.util.EditDistance;
+
+class EditDistanceTest {
+
+	static main(args) {
+		int ed = EditDistance.editDistance (args[0],args[1])
+		println("La distancia entre ${args[0]} y ${args[1]} es: $ed");
+	
+	}
+
+}
